051f68c7d8ed7656c0b852f367d9dc5486031fe3
Peter Williams <pedroeldiablo@yahoo.com>
2018-01-04 00:55:46 +0000

    React router set-up

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default () => {
+  return (
+    <div>
+      Secret Crushes
+      <ul>
+        <li>Ace Era</li>
+        <li>Zander Craze</li>
+        <li>Peto Coast</li>
+      </ul>
+    </div>
+  );
+};
